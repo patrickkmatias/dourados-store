866fcd0f920d4d9bf7c973831398fb7776a38119
Patrick M <patrickmatias.silva@hotmail.com>
2023-03-22 12:40:27 +0000

    feat: favicon change, added global utility classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/logo.svg" />
             </Head>
             <main></main>
         </>
