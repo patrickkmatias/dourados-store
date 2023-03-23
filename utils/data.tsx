7f1501cc1fb84b6912f912f6bb5eb59ea09cc5b4
Patrick <99044724+pMattheew@users.noreply.github.com>
2023-03-23 13:16:57 -0300

    feat: layout component and minor code refactor (#9)

---
+++ utils/data.tsx
@@ -0,0 +1,52 @@
+import { ProductModel } from "@/models/product";
+
+export const products: ProductModel[] = [
+    {
+        id: 1,
+        name: 'Hair clipper',
+        slug: 'hair-clipper',
+        price: 90,
+        imageUrl:
+            'https://images.unsplash.com/photo-1621607512022-6aecc4fed814?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+    },
+    {
+        id: 2,
+        name: 'Razor blade',
+        slug: 'razor-blade',
+        price: 30,
+        imageUrl:
+            'https://images.unsplash.com/photo-1634484144875-b5ef32d2fe92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    },
+    {
+        id: 3,
+        name: 'Hair mask',
+        slug: 'hair-mask',
+        price: 60,
+        imageUrl:
+            'https://images.unsplash.com/photo-1608248543803-ba4f8c70ae0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=753&q=80',
+    },
+    {
+        id: 1,
+        name: 'Hair clipper',
+        slug: 'hair-clipper',
+        price: 90,
+        imageUrl:
+            'https://images.unsplash.com/photo-1621607512022-6aecc4fed814?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+    },
+    {
+        id: 2,
+        name: 'Razor blade',
+        slug: 'razor-blade',
+        price: 30,
+        imageUrl:
+            'https://images.unsplash.com/photo-1634484144875-b5ef32d2fe92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    },
+    {
+        id: 3,
+        name: 'Hair mask',
+        slug: 'hair-mask',
+        price: 60,
+        imageUrl:
+            'https://images.unsplash.com/photo-1608248543803-ba4f8c70ae0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=753&q=80',
+    },
+];
