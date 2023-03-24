2601a00d64415f5ec00176d72c53b579874fe33c
Patrick M <patrickmatias.silva@hotmail.com>
2023-03-24 02:22:12 +0000

    feat: no more products UI feedback

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
+import { StoreContext } from '@/contexts/StoreContext';
+import { useContext } from 'react';
 import { products } from '@/utils/data';
 import Layout from '@/components/Layout';
 import Product from '@/components/Product';
 
 export default function Home() {
+    const { state } = useContext(StoreContext);
+    const {
+        cart: { cartItems },
+    } = state;
+
+    // MUST be changed when receiving external data,
+    // preferably with greater than '>'
+    const hasProducts = cartItems.length === 3 ? false : true;
+
     return (
         <>
             <Layout title="">
-                <ul className="w-full px-4 pt-8 md:px-8 flex flex-col md:flex-row flex-wrap justify-around">
+                <ul className="w-full px-4 pt-8 md:px-8 flex flex-col md:flex-row flex-wrap md:justify-around">
                     {products.map((prod, i) => {
                         return (
                             <li key={i} className="w-full md:w-[45%] mb-8">
@@ -14,6 +25,20 @@ export default function Home() {
                             </li>
                         );
                     })}
+                    {/* No more products UI feedback */}
+                    {!hasProducts ? (
+                        <>
+                            <h2 className="text-xl mb-4 text-center">
+                                Whoa! There are no more products in the store
+                                for you!
+                            </h2>
+                            <h3 className="px-8 mb-8 font-light text-sm text-center text-gray-300">
+                                You're in the right way!
+                            </h3>
+                        </>
+                    ) : (
+                        <></>
+                    )}
                 </ul>
             </Layout>
         </>
