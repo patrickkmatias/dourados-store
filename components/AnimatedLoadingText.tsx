b067a3333f1bacfe1a879da3ea137dd5a817accf
Patrick M <patrickmatias.silva@hotmail.com>
2023-03-27 06:31:56 +0000

    feat: loading checkout button

--- components/AnimatedLoadingText.tsx
+++ components/AnimatedLoadingText.tsx
@@ -1,15 +1,21 @@
-export default function AnimatedLoadingText() {
+export default function AnimatedLoadingText(props: { button?: boolean }) {
     return (
-        <div className="loading-container">
-            <div className="loading-text">
-                <span>L</span>
-                <span>O</span>
-                <span>A</span>
-                <span>D</span>
-                <span>I</span>
-                <span>N</span>
-                <span>G</span>
-            </div>
+        <div className={props.button ? 'loading-container w-full h-[40px] md:h-full !mx-0 flex justify-start items-center' : 'loading-container'}>
+            {props.button ? 
+                <div className="ml-4 text-sm">Loading...</div>
+                :
+                <div className='loading-text'>
+                    <span>L</span>
+                    <span>O</span>
+                    <span>A</span>
+                    <span>D</span>
+                    <span>I</span>
+                    <span>N</span>
+                    <span>G</span>
+                </div>
+
+            }
+            
         </div>
     );
 }
