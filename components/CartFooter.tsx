fe1feea6306ff6c891d84080f2633ecf44776db1
Patrick M <patrickmatias.silva@hotmail.com>
2023-03-27 08:53:52 +0000

    feat: responsive limit for desktops

--- components/CartFooter.tsx
+++ components/CartFooter.tsx
@@ -13,7 +13,7 @@ export default function CartFooter(props: ProductModel[]) {
     const total = sumPrices(cartItems);
 
     return (
-        <footer className="w-full p-8 pt-0 md:pt-8 flex flex-col md:flex-row fixed bottom-0 left-0 z-50 bg-black rounded-tl-3xl rounded-tr-3xl border-t-4 border-t-dourados-golden shadow-xl">
+        <footer className="w-full max-w-screen-xl p-8 pt-0 md:pt-8 flex flex-col md:flex-row fixed bottom-0 left-0 z-50 bg-black rounded-tl-3xl rounded-tr-3xl border-t-4 border-t-dourados-golden shadow-xl translate-center-x">
             <div className="flex-grow-[2]">
                 <table className="w-full md:w-1/3 my-4">
                     <tbody>
